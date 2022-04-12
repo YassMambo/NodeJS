f69142280056f66844a58ccf4eecd78cf650252a
unknown <yasser.elkannafi@gmail.com>
2022-04-12 12:20:07 +0200

    modification

--- controller/userController.js
+++ controller/userController.js
@@ -1,31 +1,37 @@
+var users = [
+    {
+        id: '1',
+        firstname: 'Mambo',
+        lastname: 'Cubano',
+        age: '25'
+    },
+    {
+        id: '2',
+        firstname: 'Esteban',
+        lastname: 'Cubano',
+        age: '19'
+    },
+    {
+        id: '3',
+        firstname: 'Yanko',
+        lastname: 'Cubano',
+        age: '22'
+    }
+]
 module.exports = {
+  
     user: (req, res) => {
-        var user = 'Yasser'
-        res.status(200).render('index', {
+        var id = req.params.id;
+        var user = users.find(u => u.id == id);
+        if (user == null || user == undefined) {
+            return res.status(404).render('error', {});
+        }
+       return res.status(200).render('user', {
             user
         })
     }, 
     users: (req, res) => {
-        var users = [
-            {
-                id: '1',
-                firstname: 'Mambo',
-                name: 'Cubano',
-                age: '25'
-            },
-            {
-                id: '2',
-                firstname: 'Esteban',
-                name: 'Cubano',
-                age: '19'
-            },
-            {
-                id: '3',
-                firstname: 'Yanko',
-                name: 'Cubano',
-                age: '22'
-            }
-        ]
+       
         res.status(200).render('users', {
             users
         })
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,29 @@
 var express = require('express');
+
 var router = express.Router();
+
 const userController = require('../controller/userController')
+
 /* GET home page. */
 
 
-exports.router = (function (){
-  const router = express.Router()
-  router.route('usu').get(userController.users)
-  
-})()
+
+
+// exports.router = (function (){
+
+// const router = express.Router()
+
+// router.route('usu').get(userController.users)
+
+// return router
+
+// })()
+
+
+
+router.route('').get(userController.users);
+router.route('/user/:id').get(userController.user)
+
+
 
 module.exports = router;
